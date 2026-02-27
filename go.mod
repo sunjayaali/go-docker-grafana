de769491fe5c0fafb18f6a6a4803a34d558a3cf4
Ali Sunjaya <ali.sunjayaa@gmail.com>
2026-02-27 11:59:08 +0700

    go app

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go-app
 
 go 1.25.1
 
-require github.com/pkg/errors v0.9.1 // indirect
+require (
+	github.com/mroth/weightedrand/v3 v3.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/samber/lo v1.52.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/mroth/weightedrand/v3"
+	"github.com/samber/lo"
 )
 
 func main() {
-	for c := range time.NewTicker(time.Second).C {
-		fmt.Println(c.Format(time.RFC3339))
+	logger := slog.New(
+		slog.NewJSONHandler(os.Stdout, nil),
+	)
+
+	chooser := lo.Must(weightedrand.NewChooser(
+		weightedrand.NewChoice(slog.LevelInfo, 5),
+		weightedrand.NewChoice(slog.LevelWarn, 3),
+		weightedrand.NewChoice(slog.LevelError, 2),
+	))
+
+	for range time.NewTicker(500 * time.Millisecond).C {
+		level := chooser.Pick()
+		logger.Info("",
+			slog.String("level", level.String()),
+			slog.String("level2", level.String()),
+		)
 	}
 	http.ListenAndServe(":8080", nil)
 }
